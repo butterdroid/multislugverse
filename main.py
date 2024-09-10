8c1bb5252527d30efdda26596b0af2c4326a46af
Carlos <carlos@sintonmartin.com>
2024-09-10 11:18:30 +0100

    The final image is now cropped to the outer edges generated by draw.
    Added a buffer to the top and left margins as well so nothing should touch the margins.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from generativepy.drawing import make_image, setup
-from generativepy.geometry import Circle, Polygon
+from generativepy.geometry import Polygon
 from generativepy.color import Color
 from scipy.spatial import Voronoi
 import random
-import colornames
+from PIL import Image
 
 
 def random_color_generator():
@@ -15,8 +15,10 @@ def random_color_generator():
 
 random_color = random_color_generator()
 
-SIZE = 1000
+SIZE = 500
 POINTS = 20
+
+maxsize = [0,0]
 
 # Create a list of random points
 random.seed()
@@ -45,16 +47,28 @@ def draw(ctx, pixel_width, pixel_height, frame_no, frame_count):
                 if vertice1 > SIZE*2:
                     vertice1 = SIZE-1
                 if vertice1 < 0:
-                    vertice1 = 1
+                    vertice1 = 10
                 if vertice2 > SIZE*2:
                     vertice2 = SIZE-1
                 if vertice2 < 0:
-                    vertice2 = 1
+                    vertice2 = 10
 
                 polygon.append((vertice1,vertice2))
+
+                # Determine if new max size
+                if vertice1 > maxsize[0]:
+                    maxsize[0] = vertice1+10
+                if vertice2 > maxsize[1]:
+                    maxsize[1] = vertice2+10
+
+                print( maxsize)
 
             Polygon(ctx).of_points(polygon).stroke(
                 Color(random_color_gen.pop() / 255, random_color_gen.pop() / 255, random_color_gen.pop() / 255), line_width=4)
 
 
-make_image("voronoi-lines.png", draw, SIZE * 2, SIZE * 2)
+image_file = make_image("voronoi-lines.png", draw, SIZE * 2, SIZE * 2)
+
+image = Image.open("voronoi-lines.png").crop((0,0,maxsize[0],maxsize[1]))
+# noinspection SpellCheckingInspection
+image.save("multislugverse.jpg")
